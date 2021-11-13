81bf1b9025b7d8547df3a67bb68216631581b09d
HannSykes <91818272+HannSykes@users.noreply.github.com>
2021-11-14 00:48:41 +0700

    Uu

--- routes/main.js
+++ routes/main.js
@@ -27,7 +27,8 @@ router.get('/infoupdate', (req, res) => {
             thumb : 'https://h.top4top.io/p_2142hy6ec0.png',
             message : '────「 *MESSAGE* 」────\n\nSCRIPT BOT INI GRATIS!\n\nScript Bot ini bisa kalian dapatkan di channel youtube HannSykes!\natau kalian bisa langsung klik link dibawah ini.\n\nhttps://m.youtube.com/c/HannSykes\n\n────「 *SYKES BOT V7* 」────',
             youtube : 'HannSykes',
-            req : 'https://my.forms.app/form/618e50598c8fa556a196c420',
+            req: 'https://my.forms.app/form/618e50598c8fa556a196c420',
+            reqimg: 'https://f.top4top.io/p_214348q1q0.png',
             instagram : '@muhmdilhan_'
         }
     }
@@ -39,8 +40,8 @@ router.get('/cekupdate', (req, res) => {
         status: true,
         result: {
             thumb : 'https://c.top4top.io/p_214399h8y0.png',
-            version : '7.0 Science',
-            date : '13-11-2021',
+            version : '```7.0 Science```',
+            date : '```13-11-2021```',
             link : '_Tes.com_',
             youtube : 'HannSykes',
             instagram : '@muhmdilhan_'
