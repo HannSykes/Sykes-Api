43ca3b385655bd8dc906af378dc713c6f6870fe1
HannSykes <91818272+HannSykes@users.noreply.github.com>
2021-11-14 15:23:31 +0700

    Y

--- routes/main.js
+++ routes/main.js
@@ -23,7 +23,7 @@ router.get('/infoupdate', (req, res) => {
     config = {
         status: true,
         result: {
-            ilhansykes : '⚡ *SYKES BOT UPDATE* ⚡\n```Version : 7.0 beta```\n```Update Date : Monday, 10 Oktober 2021 | 23:00 WIB```\n```Total Features : 265 ```\n```Error Features : 0 ```\n\n╭─────≽「 *WHATS NEW?* 」\n┴\n\n┠≽ [NEW] BAN & UNBAN USER\n┠≽ [NEW] CUSTOM TEXT WELCOME\n┠≽ [NEW] DOWNLOADER V2\n┠≽ [NEW] VOTING GROUP\n┠≽ [NEW] COGAN & CECAN\n┠≽ [NEW] DARKJOKES & MEME V2\n┠≽ [NEW] TOURL\n┠≽ [NEW] REVOKE LINK GROUP\n┠≽ [NEW] LISTVN\n┠≽ [NEW] MENU LOCATION\n┠≽ [NEW] MEME GENERATOR\n┠≽ [NEW] AFK\n┠≽ [NEW] BUG STORY\n┠≽ [NEW] STORYWA\n┠≽ [NEW] ANTI DELETE\n┠≽ [NEW] ISLAM MENU\n┠≽ [NEW] RESTART & SHUTDOWN\n┠≽ [NEW] SEMOJI\n┠≽ [NEW] ANYMORE.\n┬\n╰────────────────────────\n\n͏͏͏͏͏͏͏͏͏͏͏͏͏͏╭─────≽「 *INFORMATION* 」\n┴\n┠≽ Jika menemukan bug silahkan lapor!\n┠≽ Untuk Sewa BOT silahkan chat owner!\n┬\n╰────────────────────────\n\n*CARA LAPOR BUG?*\nketik #laporbug [text] atau #report [text]\nContoh :\n#laporbug kak fitur pinterest ga respon!\n\n\n\nArigathanks :))\n\n~ HannSykes',
+            ilhansykes : 'SEDANG DALAM PEMBUATAN CHANGELOG!',
             thumb : 'https://h.top4top.io/p_2142hy6ec0.png',
             message : '────「 *MESSAGE* 」────\n\nSCRIPT BOT INI GRATIS!\n\nScript Bot ini bisa kalian dapatkan di channel youtube HannSykes!\natau kalian bisa langsung klik link dibawah ini.\n\nhttps://m.youtube.com/c/HannSykes\n\n────「 *SYKES BOT V7* 」────',
             youtube : 'HannSykes',
@@ -42,7 +42,7 @@ router.get('/cekupdate', (req, res) => {
             thumb : 'https://c.top4top.io/p_214399h8y0.png',
             version : '```7.0 Science```',
             date : '```13-11-2021```',
-            link : '_Tes.com_',
+            link : '_https://youtu.be/TwQntGx7mhM_',
             youtube : 'HannSykes',
             instagram : '@muhmdilhan_'
         }
