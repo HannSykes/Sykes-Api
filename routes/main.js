f6a9dca65b490fbdcb54a8cf468b02aa589ccea8
HannSykes <91818272+HannSykes@users.noreply.github.com>
2021-11-27 03:19:21 +0700

    Y

--- routes/main.js
+++ routes/main.js
@@ -42,7 +42,7 @@ router.get('/cekupdate', (req, res) => {
             thumb : 'https://c.top4top.io/p_214399h8y0.png',
             version : '8.0 Science',
             date : '29-11-2021',
-            link : '_https://youtu.be/MtZl9KwUmQc_',
+            link : '_Comming Soon!_',
             grupwa : 'https://chat.whatsapp.com/JnJZlnxmTnR5mpDDlbmgKR',
             youtube : 'HannSykes',
             instagram : '@muhmdilhan_'
