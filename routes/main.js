d16ce32767ae321b78a37270d84b3c415c92fdc2
HannSykes <91818272+HannSykes@users.noreply.github.com>
2021-12-21 16:42:49 +0700

    Y

--- routes/main.js
+++ routes/main.js
@@ -4,7 +4,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res) => {
-    res.redirect('https://hannsykes.github.io/IlhanSykes/')
+    res.redirect('http://ilhansykes.6te.net/')
 })
 
 router.get('/api', (req, res) => {
